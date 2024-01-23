01e0196bea8f7aee62ca79abd10fc2dfffb560c4
mehedi-hasan <mehediinform@gmail.com>
2024-01-24 01:18:36 +0600

    Second Commit Searchview_Actionbar

--- app/src/main/java/com/example/searchview_actionbar/MainActivity.java
+++ app/src/main/java/com/example/searchview_actionbar/MainActivity.java
@@ -3,12 +3,83 @@ package com.example.searchview_actionbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private ListView listView;
+
+    private String[] countryName;
+
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.setTitle("Country Name");
+
+
+        listView = findViewById(R.id.listView_Id);
+
+        countryName = getResources().getStringArray(R.array.county_names);
+
+        adapter = new ArrayAdapter<String>(MainActivity.this,R.layout.sample_layout,R.id.textView_2,countryName);
+
+        listView.setAdapter(adapter);
+
+
+        listView.setOnItemClickListener(this);
+
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        String value = adapter.getItem(position);
+
+        Toast.makeText(getApplicationContext(),value,Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_layout,menu);
+
+        MenuItem menuItem = menu.findItem(R.id.searchView_Id);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                adapter.getFilter().filter(newText);
+
+
+                return false;
+            }
+        });
+
+
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
